d208569c89a4e92f9cb522045a43bf0d26b84e1f
pyakurelprajal <pyakurelprajal@gmail.com>
2024-03-12 18:25:39 +0545

    admin setup

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\FrontPageController;
+use App\Http\Controllers\Admin\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\AdminController;
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,9 +17,33 @@ use App\Http\Controllers\FrontPageController;
 |
 */
 
-//Route::get('/', function () {
-//    return view('website.layouts.master');
-//});
-Route::get('/', [FrontPageController::class, 'index'])->name('home');
-Route::get('/about', [FrontPageController::class, 'about'])->name('aboutus');
+Route::get('/', function () {
+    return view('welcome');
+});
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+//admin
+
+Route::namespace('Admin')->prefix('admin')->name('admin.')->group(function () {
+    Route::namespace('Auth')->group(function () {
+        // Login Routes...
+        Route::get('login', [AuthenticatedSessionController::class, 'create'])->name('auth.login');
+
+        Route::post('login', [AuthenticatedSessionController::class, 'store']);
+//        Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
+    });
+    Route::get('dashboard',[AdminController::class,'admin'])->name('dashboard');
+    Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
+
+
+});
